b7c3f5793b690b3e440a96b87d4a3e12e351a6ea
Natalia <ntl9999@hotmail.com>
2020-09-04 10:39:56 -0300

    json7

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   plugins: [
     new Dotenv({
-      path: path.resolve(__dirname, "./.env"),
+      path: path.resolve(__dirname, ".env"),
     }),
   ],
   devtool: "source-map",
